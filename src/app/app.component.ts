57fb85f78d89b6057cb9d65a491f7c6af414ae7d
Prasthuth Pradeep <prasthuth@gmail.com>
2024-08-17 23:59:58 +0530

    First commit - Setup Angular / Angular Material

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatIconModule, MatDividerModule, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'angular-18-m3';
+}
